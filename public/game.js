d39880c00122dc7eb6b70c8da7fee1c43571e780
rukevwe ubioworo <ubiorukevwe@gmail.com>
2025-06-30 16:50:15 +0000

    Add new file

---
+++ public/game.js
@@ -0,0 +1,12 @@
+// Simple game.js starter script for Road Runner Shooter
+
+console.log('Game script loaded');
+
+function startGame() {
+  alert('Welcome to Road Runner Shooter! Game is starting...');
+}
+
+window.onload = () => {
+  // You can hook this to a button or start automatically
+  startGame();
+};
